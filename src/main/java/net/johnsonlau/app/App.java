2087f42ea9386ea9339f605094c5c3993d6f1f9c
Johnson Lau <gatement@gmail.com>
2015-08-09 07:47:09 +0800

    go

--- src/main/java/net/johnsonlau/app/App.java
+++ src/main/java/net/johnsonlau/app/App.java
@@ -1,13 +1,42 @@
 package net.johnsonlau.app;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
 import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
 
 public class App 
 {
     public static void main(String[] args)
     {
-        System.out.println("Good night, Johnson!");
-        get("/", (req, res) -> "123");
-        get("/hello", (req, res) -> "Hello World");
+        System.out.println("start...");
+
+        get("/", (req, res) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("fruits", Arrays.asList("apple", "banana", "orange", "peach"));
+            return new ModelAndView(attributes, "fruitPicker.ftl");
+        }, new FreeMarkerEngine());
+
+        post("/favorite_fruit", (req, res) -> {
+            final String fruit = req.queryParams("fruit");
+            if(fruit == null)
+                return "Why don't you pick one?";
+            else
+                return "Your favorite fruit is " + fruit;
+        });
+
+        get("/echo/:thing", (req, res) -> "echo: " + req.params(":thing"));
+
+        get("/hello", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Have a nice day, Johnson!");
+
+            // The hello.ftl file is located in directory:
+            // src/main/resources/spark/template/freemarker
+            return new ModelAndView(attributes, "hello.ftl");
+        }, new FreeMarkerEngine());
     }
 }
---
+++ src/main/resources/spark/template/freemarker/hello.ftl
@@ -0,0 +1,2 @@
+<h4>${message}</h4>
+<h5>The above text is set using a FreeMarkerEngine</h5>
