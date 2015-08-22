8121f788d6d34de74f452e463fe3f4ca3e2ccee1
Johnson Lau <gatement@gmail.com>
2015-08-22 11:51:34 +0800

    go

--- src/main/java/net/johnsonlau/app/App.java
+++ src/main/java/net/johnsonlau/app/App.java
@@ -1,42 +1,51 @@
 package net.johnsonlau.app;
 
-import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Date;
 
 import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCursor;
+import org.bson.Document;
+
 public class App 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         System.out.println("start...");
+        System.out.println("start..." + (new Date()).toString());
+
+        MongoClient client = new MongoClient();
+        MongoDatabase database = client.getDatabase("test");
+        MongoCollection collection = database.getCollection("test");
+        collection.drop();
+        collection.insertOne(new Document("name", "Johnson").append("age", 23));
+        collection.insertOne(new Document("name", "Anne").append("age", 22));
+
+        MongoCursor<Document> cur = collection.find(Document.class).iterator();
+        cur.next();
+        Document doc = cur.next();
 
         get("/", (req, res) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("fruits", Arrays.asList("apple", "banana", "orange", "peach"));
-            return new ModelAndView(attributes, "fruitPicker.ftl");
+            //Document doc = (Document)collection.find().first();
+            List<Document> docs = new ArrayList();
+            collection.find().into(docs);
+            Map<String, Object> map = new HashMap<>();
+            map.put("docs", docs);
+
+            return new ModelAndView(map, "hello.ftl");
         }, new FreeMarkerEngine());
 
-        post("/favorite_fruit", (req, res) -> {
-            final String fruit = req.queryParams("fruit");
-            if(fruit == null)
-                return "Why don't you pick one?";
-            else
-                return "Your favorite fruit is " + fruit;
-        });
-
-        get("/echo/:thing", (req, res) -> "echo: " + req.params(":thing"));
-
-        get("/hello", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Have a nice day, Johnson!");
-
-            // The hello.ftl file is located in directory:
-            // src/main/resources/spark/template/freemarker
-            return new ModelAndView(attributes, "hello.ftl");
-        }, new FreeMarkerEngine());
+        //Thread.sleep(1000000000);
+        System.out.println("end...");
     }
 }
--- src/main/resources/spark/template/freemarker/hello.ftl
+++ src/main/resources/spark/template/freemarker/hello.ftl
@@ -1,2 +1,5 @@
-<h4>${message}</h4>
+<#list docs as doc>
+    <h4>${doc["name"]}</h4>
+</#list>
+
 <h5>The above text is set using a FreeMarkerEngine</h5>
